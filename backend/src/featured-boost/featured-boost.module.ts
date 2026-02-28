eb1cbf37a16bd2f892a05f2b49bda15ec7c1e501
pizn-01 <airafadil619@gmail.com>
2026-03-01 03:01:05 +0500

    fix(backend): add missing AuthModule import to FeaturedBoostModule

--- backend/src/featured-boost/featured-boost.module.ts
+++ backend/src/featured-boost/featured-boost.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { FeaturedBoostService } from './featured-boost.service';
 import { FeaturedBoostController } from './featured-boost.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-    imports: [PrismaModule],
+    imports: [PrismaModule, AuthModule],
     controllers: [FeaturedBoostController],
     providers: [FeaturedBoostService],
     exports: [FeaturedBoostService],
