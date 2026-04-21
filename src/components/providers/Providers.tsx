0e6fbc6f17bd7c073c568b114aa2d32b577008bd
pizn-01 <airafadil619@gmail.com>
2026-04-21 21:02:48 +0500

    QA work

--- src/components/providers/Providers.tsx
+++ src/components/providers/Providers.tsx
@@ -1,18 +1,12 @@
 "use client"
 
 import { CompareProvider } from "@/context/CompareContext"
-import dynamic from "next/dynamic"
 
-const CompareDrawer = dynamic(
-    () => import("@/components/features/CompareDrawer").then(mod => mod.CompareDrawer),
-    { ssr: false }
-)
 
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
         <CompareProvider>
             {children}
-            <CompareDrawer />
         </CompareProvider>
     )
 }
