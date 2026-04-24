d866a3d44613535b12af6a57e462576576514051
pizn-01 <airafadil619@gmail.com>
2026-04-24 23:43:20 +0500

    Enquire now update

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -67,8 +67,8 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
             <MotionButton
                 className={cn(buttonVariants({ variant, size, shape, className }))}
                 ref={ref as any}
-                whileHover={{ scale: 1.05 }}
-                whileTap={{ scale: 0.95 }}
+                whileHover={{ scale: 1.02 }}
+                whileTap={{ scale: 0.98 }}
                 transition={{ type: "spring", stiffness: 400, damping: 17 }}
                 {...(props as any)}
             />
