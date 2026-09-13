0b8a709669dfbeef8b6ad6bb0b133c6b11cb21e3
carmaziums <carmaziums@gmail.com>
2026-09-13 02:24:06 +0100

    Hook free-listing entitlement into retail publish only

--- backend/src/listings/listings.module.ts
+++ backend/src/listings/listings.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ListingsService } from './listings.service';
 import { ListingsController } from './listings.controller';
@@ -8,11 +8,19 @@ import { SellersModule } from '../sellers/sellers.module';
 import { ScraperModule } from '../scraper/scraper.module';
 import { NotificationsModule } from '../notifications/notifications.module';
 import { TradeListingAccessGuard } from '../auctions/trade-access.guard';
+import { FreeListingsModule } from '../free-listings/free-listings.module';
+import { FreeListingPublishMiddleware } from '../free-listings/free-listing-publish.middleware';
 
 @Module({
-  imports: [PrismaModule, AuthModule, SellersModule, ConfigModule, ScraperModule, NotificationsModule],
+  imports: [PrismaModule, AuthModule, SellersModule, ConfigModule, ScraperModule, NotificationsModule, FreeListingsModule],
   controllers: [ListingsController],
   providers: [ListingsService, TradeListingAccessGuard],
   exports: [ListingsService],
 })
-export class ListingsModule { }
+export class ListingsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(FreeListingPublishMiddleware)
+      .forRoutes({ path: 'listings/:id/publish', method: RequestMethod.POST });
+  }
+}
