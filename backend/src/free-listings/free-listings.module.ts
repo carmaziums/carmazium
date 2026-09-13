8302647e39e1b0f521695f88624d9dc393e8b017
carmaziums <carmaziums@gmail.com>
2026-09-13 09:44:47 +0100

    Import auth guards for free listing admin endpoints

--- backend/src/free-listings/free-listings.module.ts
+++ backend/src/free-listings/free-listings.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AdminFreeListingsController } from './free-listings.controller';
 import { FreeListingGrantInterceptor } from './free-listing-grants.interceptor';
 import { FreeListingGrantsService } from './free-listing-grants.service';
 
 @Module({
-    imports: [PrismaModule],
+    imports: [PrismaModule, AuthModule],
     controllers: [AdminFreeListingsController],
     providers: [
         FreeListingGrantsService,
