81f579bdb2ee0b63cb0bb6310f34e42bc6fc83ad
carmaziums <carmaziums@gmail.com>
2026-09-19 01:44:23 +0100

    fix linked auction regression assertion import

--- backend/src/listings/listings.service.spec.ts
+++ backend/src/listings/listings.service.spec.ts
@@ -1,3 +1,4 @@
+import { BadRequestException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { ConfigService } from '@nestjs/config';
 import { ListingsService } from './listings.service';
