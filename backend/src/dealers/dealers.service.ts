98e232c337e4027c0370e4cba1ce928a833ce5b1
carmaziums <carmaziums@gmail.com>
2026-09-23 20:15:15 +0100

    Block 6: expose dealer inventory permission

--- backend/src/dealers/dealers.service.ts
+++ backend/src/dealers/dealers.service.ts
@@ -32,6 +32,7 @@ export class DealersService {
         'PAY_AUCTION_FEE',
         'MANAGE_CRM',
         'MANAGE_OFFERS',
+        'MANAGE_INVENTORY',
         'VIEW_PURCHASES',
         'VIEW_ANALYTICS',
         'MANAGE_TEAM',
